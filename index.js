bb024241f4d7871e96f9759df26747f447d11e54
annecie <ajbexcelsior@gmail.com>
2021-10-27 15:11:57 +0000

    add changes

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 //1
 function betweenFiveAndTen(){
-  
+  let func=0
+  if(func >=5 && func <=10){
+  console.log(true)    
+  }else{
+  console.log(false);      
+  }
 }
+console.log((betweenFiveAndTen(10))
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(func){
+  if(func >=10 && func <=30){
+  return true
+  }else{
+  return false    
 }
-
+console.log((betweenTenAndThirty(40))
 //3
 function betweenThirtyAndFifty(){
-  
+ let func=0
+  if(func >=5 || func <=10){
+  console.log(true)    
+  }else{
+  console.log(false);    
 }
 
 
 //4
 function betweenThirtyFiftySeventy(){
+  if (number>= 0 && number<=30 || number>=50 && number <=70{
+  return true
+  }else{
+  return false  
+  }
   
 }
+console.log(betweenThirtyAndFifty(48))
 
 //5
 function betweenTwentyFiftySixty(){
